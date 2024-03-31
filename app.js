78b8b64fba9c97f81674cb7d0926dc3239522a6f
ELIASyASSER <149430963+ELIASyASSER@users.noreply.github.com>
2024-04-01 01:15:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let mainImg = document.querySelector(".main");
+let cont = document.querySelector(".container");
+function phones(phone){
+    mainImg.src = phone;
+}
+function theme(x){
+    cont.style.background = x;
+}
